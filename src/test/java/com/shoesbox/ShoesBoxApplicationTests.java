c94c20916e5258cd2d321685f0739b2be5dce40c
Elrendar <guuto9@gmail.com>
2022-08-26 22:35:07 +0900

    Base Spring Project
    Spring Web
    Spring Security
    validation
    lombok
    H2 Database
    MySQL Driver
    Spring Data JPA

---
+++ src/test/java/com/shoesbox/ShoesBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoesbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
