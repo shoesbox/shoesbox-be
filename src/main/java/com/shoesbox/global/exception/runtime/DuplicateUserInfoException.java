d3c516c2a7f16d5009789e05bdde6f295a291758
chanho <guuto9@gmail.com>
2022-08-30 20:39:19 +0900

    feat: Add ExceptionHandler and ResponseWrapper
    커스텀 예외 목록:
    DuplicateUserInfoException
    InvalidJWTException
    PostNotFoundException
    RefreshTokenNotFoundException
    UnAuthorizedException
    
    ApiError는 예외 메시지를 일관된 형태로 반환하기 위한 래퍼 클래스
    
    ResponseWrapper는 api 응답을 일관된 형태로 반환하기 위한 래퍼 클래스
    
    ResponseWrapper 사용법:
    
    Controller의 반환값은
    ResponseEntity<ResponseWrapper<반환할 객체타입>>
    return 값은
    ResponseWrapper.ok(반환할 객체);
    
    Resolves: #8

---
+++ src/main/java/com/shoesbox/global/exception/runtime/DuplicateUserInfoException.java
@@ -0,0 +1,14 @@
+package com.shoesbox.global.exception.runtime;
+
+/**
+ * 회원가입 시 DB에 username, email, nickname이 이미 존재할 경우 발생
+ */
+public class DuplicateUserInfoException extends RuntimeException {
+    public DuplicateUserInfoException(String message) {
+        super(message);
+    }
+
+    public DuplicateUserInfoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
