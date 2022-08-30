50bb8f448cdb5b0dd07b28017a311c6e31284a48
myeongbaek <moungbak421@daum.net>
2022-08-30 10:41:13 +0900

    #6 feat : Post.java 엔터티 추가, Timestamped 상속
    - post의 엔터티 어노테이션 및 컬럼이 추가되었습니다.
    - content를 contents로 복수형으로 작성이 됐습니다.
    
    -> images의 기능이 무엇인지 논의가 필요합니다. 제 1 정규화가 필요해 보입니다.

--- src/main/java/com/shoesbox/post/Post.java
+++ src/main/java/com/shoesbox/post/Post.java
@@ -1,4 +1,59 @@
 package com.shoesbox.post;
 
-public class Post {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Post extends Timestamped{
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @Column(nullable = false)
+    private boolean is_private;
+
+    @Column(nullable = false)
+    private int year;
+
+    @Column(nullable = false)
+    private int month;
+
+    @Column(nullable = false)
+    private int day;
+
+    @Column(nullable = true)
+    private List<String> images;
+
+    @Builder
+    public Post(String contents, ...){
+        this.contents = contents;
+        ...
+    }
+
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "FK_user_post"))
+//    private User user;}
+//    public void mapToUser(User userFoundById) {
+//        this.user = userFoundById;
+//        userFoundById.mapToContents(this);
+//    }
+//    public void mapToPost(String postImageURL) {
+//        this.imageUrl = postImageURL;
+//    }
+
 }
+
