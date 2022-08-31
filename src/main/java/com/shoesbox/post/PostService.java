a656d31bd343fa8c53b45c713b5efddef9d1b531
myeongbaek <moungbak421@daum.net>
2022-08-31 14:07:46 +0900

    #6 feat : PostService.java 게시물 생성 구현완료

--- src/main/java/com/shoesbox/post/PostService.java
+++ src/main/java/com/shoesbox/post/PostService.java
@@ -11,8 +11,14 @@ public class PostService {
     private final PostRepository postRepository;
 
     @Transactional
-    public Post createPost(PostRequestDto postRequestDto){
+    public Post createPost(PostRequestDto dto){
         Post post = Post.builder()
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .is_private(dto.is_private())
+                .year(dto.getYear())
+                .month(dto.getMonth())
+                .day(dto.getDay())
                 .build();
         postRepository.save(post);
         return post;
