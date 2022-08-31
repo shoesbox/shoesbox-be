a318c5f96050ae6e0cd7ce761979e9b09415de24
myeongbaek <moungbak421@daum.net>
2022-08-31 14:01:09 +0900

    #6 feat : PostService.java 게시물 생성 기능 구현

--- src/main/java/com/shoesbox/post/PostService.java
+++ src/main/java/com/shoesbox/post/PostService.java
@@ -1,5 +1,20 @@
 package com.shoesbox.post;
 
-public class PostService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+    private final PostRepository postRepository;
+
+    @Transactional
+    public Post createPost(PostRequestDto postRequestDto){
+        Post post = Post.builder()
+                .build();
+        postRepository.save(post);
+        return post;
+    }
 }
