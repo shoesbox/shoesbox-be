f5ed0db23c9f52d909c06d36a8101a18cdef5e39
myeongbaek <moungbak421@daum.net>
2022-08-31 14:05:24 +0900

    #6 feat : PostRequestDto.java dto 구현

--- src/main/java/com/shoesbox/post/PostRequestDto.java
+++ src/main/java/com/shoesbox/post/PostRequestDto.java
@@ -1,4 +1,15 @@
 package com.shoesbox.post;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 public class PostRequestDto {
+    private String title;
+    private String content;
+    private boolean is_private;
+    private int year;
+    private int month;
+    private int day;
 }
