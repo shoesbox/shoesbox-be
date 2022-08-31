001d51048a1900f71f533048855a4bc2ff9d04b6
myeongbaek <moungbak421@daum.net>
2022-08-31 13:57:53 +0900

    #6 feat : PostController.java 게시물 생성 api 구현

--- src/main/java/com/shoesbox/post/PostController.java
+++ src/main/java/com/shoesbox/post/PostController.java
@@ -1,4 +1,20 @@
 package com.shoesbox.post;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/posts")
 public class PostController {
+    private final PostService postService;
+    private final PostRepository postRepository;
+
+    @PostMapping
+    public String createPost(@RequestBody PostRequestDto postRequestDto){
+        this.postService.createPost(postRequestDto);
+        return "redirect:/post";
+    }
+
 }
--- src/main/java/com/shoesbox/post/PostService.java
+++ src/main/java/com/shoesbox/post/PostService.java
@@ -1,4 +1,5 @@
 package com.shoesbox.post;
 
 public class PostService {
+
 }
