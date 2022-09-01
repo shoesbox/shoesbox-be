1ebbb62b1a671b7c182fbe947066eb235f6fa4fd
myeongbaek <moungbak421@daum.net>
2022-09-01 11:32:08 +0900

    #6 feat : 병합 전 정리

--- src/main/java/com/shoesbox/ShoesBoxApplication.java
+++ src/main/java/com/shoesbox/ShoesBoxApplication.java
@@ -1,14 +1,8 @@
 package com.shoesbox;
 
-import com.shoesbox.post.Post;
-import com.shoesbox.post.PostRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import java.util.List;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -18,20 +12,4 @@ public class ShoesBoxApplication {
         SpringApplication.run(ShoesBoxApplication.class, args);
     }
 
-
-    @Bean
-    public CommandLineRunner demo(PostRepository repository) {
-        return (args) -> {
-            Post post1 = new Post("제목1", "내용1", "사진1"); // 생성
-            repository.save(post1); // 삽입
-
-            List<Post> postList = repository.findAll(); // 조회
-            for (int i = 0; i < postList.size(); i++) {
-                Post p = postList.get(i);
-                System.out.println(p.getTitle());
-                System.out.println(p.getContent());
-                System.out.println(p.getImages());
-            }
-        };
-    }
 }
