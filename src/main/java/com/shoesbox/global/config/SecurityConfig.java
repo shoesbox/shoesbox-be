2c7821a80857b6f1125e26dd87c489bab4dcae54
chanho <guuto9@gmail.com>
2022-09-03 12:49:24 +0900

    feat: Add SecurityConfig.java
    보안 설정 클래스 추가
    
    Related to: #22

---
+++ src/main/java/com/shoesbox/global/config/SecurityConfig.java
@@ -0,0 +1,97 @@
+package com.shoesbox.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    // private final TokenProvider tokenProvider;
+    // private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    // private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // 로그인 없이 h2 콘솔, 파비콘 사용 허가
+        return (web) -> web.ignoring().antMatchers("/h2-console/**"
+                , "/favicon.ico"
+                , "/error");
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000");
+        // configuration.addAllowedOrigin("http://ec2-52-79-227-137.ap-northeast-2.compute.amazonaws.com:8000/");
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                // token을 사용하는 방식이기 때문에 csrf를 disable
+                .csrf().disable()
+
+                // cors 필터 적용
+                .cors().configurationSource(corsConfigurationSource())
+
+                // // 예외처리에 직접 구현한 클래스들을 사용하도록 추가
+                // .and()
+                // .exceptionHandling()
+                // .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                // .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // 스프링 시큐리티는 기본적으로 세션을 사용
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .logout().disable()
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                // 로그인, 회원가입 등 토큰이 없을 때 요청이 들어오는 API는 permitAll
+                .and()
+                .authorizeRequests()
+
+                .antMatchers("/").permitAll()
+                .antMatchers("/api/members/auth/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/posts/**", "/api/comments/**").permitAll()
+                // .antMatchers(HttpMethod.POST, "/api/posts/**").hasAnyAuthority("ROLE_USER")
+
+                // 나머지는 전부 인증 필요
+                .anyRequest().authenticated();
+
+        // // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
+        // .and()
+        // .apply(new JwtSecurityConfig(tokenProvider));
+
+        return httpSecurity.build();
+    }
+}
