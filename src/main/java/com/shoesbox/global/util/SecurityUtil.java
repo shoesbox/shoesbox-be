7d4f1ae963a4c5283af020183edeb9d019cbe7eb
chanho <guuto9@gmail.com>
2022-09-03 15:08:54 +0900

    feat: Add SecurityUtil
    기능 추가:
    
    현재 로그인한 사용자의 정보를 가져올 수 있는 유틸 클래스 생성
    SecurityUtil.getCurrentMemberIdByLong()
    SecurityUtil.getCurrentMemberNickname()
    
    Related to: #22

--- src/main/java/com/shoesbox/global/util/SecurityUtil.java
+++ src/main/java/com/shoesbox/global/util/SecurityUtil.java
@@ -5,27 +5,38 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Slf4j
 @NoArgsConstructor
 public class SecurityUtil {
-    public static long getCurrentUserIdByLong() {
-        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    public static long getCurrentMemberIdByLong() {
+        var principal = getPrincipal();
 
+        if (principal != null) {
+            return principal.getMemberId();
+        }
+
+        return 0L;
+    }
+
+    public static String getCurrentMemberNickname() {
+        var principal = getPrincipal();
+
+        if (principal != null) {
+            return principal.getNickname();
+        }
+
+        return null;
+    }
+
+    private static CustomUserDetails getPrincipal() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null) {
             log.debug("Security Context에 인증 정보가 없습니다.");
             throw new UsernameNotFoundException(
                     "Security Context에 인증 정보가 없습니다.");
         }
-
-        long userId = 0L;
-        if (authentication.getPrincipal() instanceof UserDetails) {
-            CustomUserDetails springSecurityUser = (CustomUserDetails) authentication.getPrincipal();
-            userId = springSecurityUser.getUserId();
-        }
-
-        return userId;
+        return (CustomUserDetails) authentication.getPrincipal();
     }
 }
