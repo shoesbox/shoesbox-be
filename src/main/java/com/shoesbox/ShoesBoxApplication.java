f0c095c25ea2b1262d3b09999fa8f3384a5b8771
CINY4612 <ciy5364612@gmail.com>
2022-09-07 16:24:14 +0900

    fix: Fix TimeZone
    서버 시간 UDT에서 KDT로 변경
    
    Related to: #36

--- src/main/java/com/shoesbox/ShoesBoxApplication.java
+++ src/main/java/com/shoesbox/ShoesBoxApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class ShoesBoxApplication {
@@ -12,4 +15,8 @@ public class ShoesBoxApplication {
         SpringApplication.run(ShoesBoxApplication.class, args);
     }
 
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
