3f16d0d84eb8af5153c54485bd31068167169281
CINY4612 <ciy5364612@gmail.com>
2022-09-09 09:12:30 +0900

    feat: Add Exception handler
    응답값에 예외처리 추가
    
    Related to: #34

--- src/main/java/com/shoesbox/domain/social/SocialController.java
+++ src/main/java/com/shoesbox/domain/social/SocialController.java
@@ -1,8 +1,9 @@
 package com.shoesbox.domain.social;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.shoesbox.domain.auth.TokenDto;
+import com.shoesbox.global.common.ResponseHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +17,7 @@ public class SocialController {
 
     // 카카오에 redirect url을 통해 인가코드(AuthorizedCode) 요청 - code이름으로 받아옴
     @GetMapping("/oauth2/authorization/{provider}")
-    public TokenDto socialLogin(@RequestParam String code, @PathVariable String provider) throws JsonProcessingException {
-        return providerService.SocialLogin(code, provider);
+    public ResponseEntity<Object> socialLogin(@RequestParam String code, @PathVariable String provider) throws JsonProcessingException {
+        return ResponseHandler.ok(providerService.SocialLogin(code, provider));
     }
 }
