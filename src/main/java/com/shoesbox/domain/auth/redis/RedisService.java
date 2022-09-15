c87cb730d4deb9c360ee626ede1fc1f2c1414716
CINY4612 <ciy5364612@gmail.com>
2022-09-15 15:40:01 +0900

    Feat: Add Redis config
    redis 사용 설정
    
    Related to: #73

---
+++ src/main/java/com/shoesbox/domain/auth/redis/RedisService.java
@@ -0,0 +1,29 @@
+package com.shoesbox.domain.auth.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class RedisService {
+
+    private final RedisTemplate redisTemplate;
+
+    // key를 통해 value를 읽어들임
+    public String getData(String key){
+        return (String) redisTemplate.opsForValue().get(key);
+    }
+
+    public void setDataWithExpiration(String key, String value, Long time){
+        if(this.getData(key) != null){
+            redisTemplate.delete(key);
+        }
+        Duration expireDuration = Duration.ofSeconds(time);
+        redisTemplate.opsForValue().set(key, value, expireDuration);
+    }
+}
