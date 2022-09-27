bc2c3e8c4876faecec81364cdd57b2f188f4a870
myeongbaek <moungbak421@daum.net>
2022-09-28 05:12:42 +0900

    feature : messagedto
    #111

---
+++ src/main/java/com/shoesbox/domain/sse/MessageDto.java
@@ -0,0 +1,16 @@
+package com.shoesbox.domain.sse;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.extern.jackson.Jacksonized;
+
+@Jacksonized
+@Builder
+@Getter
+public class MessageDto {
+    String msgType;
+    String senderNickName;
+    long postId;
+    String month;
+    String day;
+}
