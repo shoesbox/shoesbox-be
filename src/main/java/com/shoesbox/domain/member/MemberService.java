de88bc3933aa76bb5383d0b71ed8c0f907837b23
myeongbaek <moungbak421@daum.net>
2022-09-29 17:32:35 +0900

    fix : 로그아웃 시 실시간 통신 목록 제거
    #111

--- src/main/java/com/shoesbox/domain/member/MemberService.java
+++ src/main/java/com/shoesbox/domain/member/MemberService.java
@@ -32,6 +32,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.shoesbox.domain.sse.SseController.sseEmitters;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -165,6 +167,8 @@ public class MemberService {
 
     @Transactional
     public Boolean logout(String email) {
+        Member member = memberRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("로그아웃 할 유저를 찾을 수 없습니다."));
+        sseEmitters.remove(member.getId());
         return redisTemplate.delete("RT:" + email);
     }
 
